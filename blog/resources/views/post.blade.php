8442e3799b26607d8bea304882a759d9b03843f9
Abdullah Shahzad <a.shahzad@uqconnect.edu.au>
2022-08-16 22:48:21 +1000

    Added progress from mac

---
+++ blog/resources/views/post.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<title>Abdullah's Laravel Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<script src="/app.js"></script>
+
+<body>
+    <article>
+        <?= $post; ?>
+    </article>
+
+    <a href="/">Go back</a>
+
+</body>
