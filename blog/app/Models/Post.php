946a9e03b0b4ce7133500abca7bc55f3ae859ac0
Abdullah Shahzad <a.shahzad@uqconnect.edu.au>
2022-08-18 09:07:34 +1000

    Post Metadata handling

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -3,9 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
 
 class Post
 {
+    public $title;
+
+    public $excerpt;
+
+    public $date;
+
+    public $body;
+
+    public function __construct($title, $excerpt, $date, $body)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+    }
+
+    public static function all(){
+        $files = File::files(resource_path("posts/"));
+
+        return array_map(fn($file) => $file->getContents(), $files);
+    }
+
     public static function find($slug) {
         if (! file_exists($path = resource_path("posts/{$slug}.html"))) {
             throw new ModelNotFoundException();
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -12,9 +12,31 @@
 */
 
 use App\Models\Post;
+use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {
-    return view('posts');
+    $files = File::files(resource_path("posts"));
+
+    dd($files);
+    $posts = [];
+
+    foreach ($files as $file) {
+        $document = YamlFrontMatter::parseFile($file);
+
+        $posts[] = new Post(
+            $document->title,
+            $document->excerpt,
+            $document->date,
+            $document->body(),
+        );
+    }
+
+
+    dd($posts);
+//    return view('posts', [
+//        'posts' => Post::all()
+//    ]);
 });
 
 Route::get('post/{post}', function ($slug) {
