11bbc2db1e844b59690a71629d2743cd6b96c05a
Abdullah Shahzad <a.shahzad@uqconnect.edu.au>
2022-08-24 11:13:26 +1000

    Associated blog posts with categories

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -12,16 +12,23 @@
 */
 
 use App\Models\Post;
+use \App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => Post::with('category')->get()
     ]);
 });
 
 Route::get('posts/{post:slug}', function (Post $post) {
     return view('post', [
         'post' => $post
+    ]);
+});
+
+Route::get('categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts
     ]);
 });
