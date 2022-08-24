25838cad5f58c16a780d8c0b607a2fbfc3b4b737
Abdullah Shahzad <a.shahzad@uqconnect.edu.au>
2022-08-24 10:34:39 +1000

    Route model binding - automatic finding of post

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -2,12 +2,12 @@
     @foreach ($posts as $post)
         <article>
             <h1>
-                <a href="/posts/{{ $post->id }}">
+                <a href="/posts/{{ $post->slug }}">
                     {{  $post->title }}
                 </a>
             </h1>
             <div>
-                {{ $post->excerpt }}
+                {!! $post->excerpt !!}
             </div>
         </article>
     @endforeach
