3257aa0d83fdf8a9cb44ddb49a9c3e0c501b25c3
Abdullah Shahzad <a.shahzad@uqconnect.edu.au>
2022-08-24 10:03:05 +1000

    Using Post Model and migration

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -2,71 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public $title;
-
-    public $excerpt;
-
-    public $date;
-
-    public $body;
-
-    public $slug;
-
-    public function __construct($title, $excerpt, $date, $slug, $body)
-    {
-        $this->title = $title;
-        $this->excerpt = $excerpt;
-        $this->date = $date;
-        $this->body = $body;
-        $this->slug = $slug;
-    }
-
-    public static function all(){
-//        Unable to clear cache with php artisan tinker within the docker container so not caching this
-//        return cache()->rememberForever('posts.all', function () {
-//            return collect(File::files(resource_path("posts")))
-//                ->map(fn($file) => YamlFrontMatter::parseFile($file))
-//                ->map(fn($document) => new Post(
-//                    $document->title,
-//                    $document->excerpt,
-//                    $document->date,
-//                    $document->slug,
-//                    $document->body()
-//                ))
-//                ->sortByDesc('date' );
-//        });
-
-        return collect(File::files(resource_path("posts")))
-            ->map(fn($file) => YamlFrontMatter::parseFile($file))
-            ->map(fn($document) => new Post(
-                $document->title,
-                $document->excerpt,
-                $document->date,
-                $document->slug,
-                $document->body()
-            ))
-            ->sortByDesc('date' );
-
-
-    }
-
-    public static function find($slug) {
-        return static::all()->firstWhere('slug', $slug);
-    }
-
-    public static function findOrFail($slug) {
-        $post = static::find($slug);
-
-        if (! $post) {
-            throw new ModelNotFoundException();
-        }
-
-        return $post;
-    }
+    use HasFactory;
 }
