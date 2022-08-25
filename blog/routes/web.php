8a30de66246b2ee7b736747f23ad058fed9b1d7f
Abdullah Shahzad <a.shahzad@uqconnect.edu.au>
2022-08-25 13:53:46 +1000

    Using controller for search2

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -11,31 +11,15 @@
 |
 */
 
+use App\Http\Controllers\PostController;
 use App\Models\Post;
 use \App\Models\Category;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    $post = Post::latest();
+Route::get('/', [PostController::class, 'index'])->name('home');
 
-    if (request('search')) {
-        $post
-            ->where('title', 'like', '%' . request('search') . '%')
-            ->orWhere('body', 'like', '%' . request('search') . '%');
-    }
-
-    return view('posts', [
-        'posts' => $post->get(),
-        'categories' => Category::all()
-    ]);
-})->name('home');
-
-Route::get('posts/{post:slug}', function (Post $post) {
-    return view('post', [
-        'post' => $post
-    ]);
-});
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
