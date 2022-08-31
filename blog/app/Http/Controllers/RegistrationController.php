3dcdca1a0ced49e44b57615849546914ae69a2ba
Abdullah Shahzad <a.shahzad@uqconnect.edu.au>
2022-08-31 12:13:55 +1000

    Failed Validation and Old input data

--- blog/app/Http/Controllers/RegistrationController.php
+++ blog/app/Http/Controllers/RegistrationController.php
@@ -16,9 +16,9 @@ class RegistrationController extends Controller
     {
         $attributes = request()->validate([
             'name' => 'required|max:255',
-            'username' => 'required|max:255|min:3',
-            'email' => 'required|email|max:255',
-            'password' => ['required', 'max:255', 'min:7'],
+            'username' => 'required|min:3|max:255|unique:users,username',
+            'email' => 'required|email|max:255|unique:users, email',
+            'password' => ['required', 'min:7', 'max:255'],
         ]);
 
         User::create($attributes);
