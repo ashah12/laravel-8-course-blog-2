a609d46633b98bf2160958c632baa2ee80aef9b2
Abdullah Shahzad <a.shahzad@uqconnect.edu.au>
2022-08-31 15:18:13 +1000

    Markup for Post comments

--- blog/app/Http/Controllers/SessionsController.php
+++ blog/app/Http/Controllers/SessionsController.php
@@ -29,7 +29,7 @@ class SessionsController extends Controller
 //        ]);
 
         session()->regenerate();
-        
+
         return redirect('/')->with('success', 'Successfully logged in!');
     }
 
