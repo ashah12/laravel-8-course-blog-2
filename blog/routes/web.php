912bb5093f751e988553be53eb867a91ad7665ad
Abdullah Shahzad <a.shahzad@uqconnect.edu.au>
2022-09-01 11:20:10 +1000

    Using/Tinkering the Mailchimp API

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -6,6 +6,25 @@ use App\Http\Controllers\RegistrationController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('ping', function () {
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us14'
+    ]);
+
+//    $response = $mailchimp->ping->get();
+//    $response = $mailchimp->lists->getAllLists();
+//    $response = $mailchimp->lists->getList('6196965d1b');
+//    $response = $mailchimp->lists->getListMembersInfo('6196965d1b');
+    $response = $mailchimp->lists->addListMember('6196965d1b', [
+        'email_address' => 'trolllevel101@gmail.com',
+        'status' => 'subscribed',
+    ]);
+    dd($response);
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
