65f8c952dba83e425e4db3b57e4cd6d0f1a6ef0f
Abdullah Shahzad <a.shahzad@uqconnect.edu.au>
2022-09-07 12:01:52 +1000

    Create a form to edit and delete posts

--- blog/resources/views/components/form/textarea.blade.php
+++ blog/resources/views/components/form/textarea.blade.php
@@ -4,10 +4,10 @@
     <x-form.label name="{{ $name }}" />
 
     <textarea class="border border-gray-200 p-2 w-full rounded"
-              name="excerpt"
+              name="{{ $name }}"
               id="{{ $name }}"
               required
-    >{{ old($name) }}</textarea>
+    >{{ $slot ?? old($name) }}</textarea>
 
     <x-form.error name="{{ $name }}" />
 </x-form.field>
