f748646c56a63cb5af220fa33fa6923af1a7fed0
Abdullah Shahzad <a.shahzad@uqconnect.edu.au>
2022-09-07 14:11:33 +1000

    Using route resource to generate the 7 resourceful controller actions routes

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -26,11 +26,12 @@ Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
 Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
 
 Route::middleware('can:admin')->group(function () {
-    Route::post('admin/posts', [AdminPostController::class, 'store']);
-    Route::get('admin/posts/create', [AdminPostController::class, 'create']);
-    Route::get('admin/posts', [AdminPostController::class, 'index']);
-    Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit']);
-    Route::patch('admin/posts/{post}', [AdminPostController::class, 'update']);
-    Route::delete('admin/posts/{post}', [AdminPostController::class, 'destroy']);
+    Route::resource('admin/posts', AdminPostController::class)->except('show');
+//    Route::post('admin/posts', [AdminPostController::class, 'store']);
+//    Route::get('admin/posts/create', [AdminPostController::class, 'create']);
+//    Route::get('admin/posts', [AdminPostController::class, 'index']);
+//    Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit']);
+//    Route::patch('admin/posts/{post}', [AdminPostController::class, 'update']);
+//    Route::delete('admin/posts/{post}', [AdminPostController::class, 'destroy']);
 });
 
